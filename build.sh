c1435bd59a0d1a8a0b6ca617615f8118d5fe7ae6
meets2tarun <meets2tarun@gmail.com>
2017-04-23 00:14:00 +0530

    Upgrade it to zesty 17.04

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ mirror=${2:-"http://archive.ubuntu.com/ubuntu/"}
 # Install language with the most popcon
 gnomelanguage=${3:-'{en}'}	#
 # Release name, used by debootstrap.  Examples: lucid, maverick, natty.
-release=${4:-yakkety}
+release=${4:-zesty}
 
 # Necessary data files
 datafiles="image-${arch}.tar.lzma sources.list"
