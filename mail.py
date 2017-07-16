a0f432547bbe9d87ddf1f6b9fa95b1526deb23e5
Xeon Zolt <xeon.harsh@gmail.com>
2017-07-17 02:08:24 +0530

    revoked older key and using new as env variable

--- mail.py
+++ mail.py
@@ -1,13 +1,18 @@
 #! /usr/bin/env python3
 import http.client
 import os
+
 conn = http.client.HTTPSConnection("api.sendgrid.com")
+
 print (os.environ["email"])
 payload = "{\"personalizations\":[{\"to\":[{\"email\":\"" + os.environ["email"] + "\"}],\"subject\":\"Your ISO is Ready\"}],\"from\":{\"email\":\"xeon.harsh@gmail.com\",\"name\":\"Meilix Generator\"},\"reply_to\":{\"email\":\"xeon.harsh@gmail.com\",\"name\":\"Meilix Generator\"},\"subject\":\"Your ISO is ready\",\"content\":[{\"type\":\"text/html\",\"value\":\"<html><p>Hi,<br>Your ISO is ready <br><br>Thank You,<br>Meilix Generator Team</p></html>\"}]}"
+
 print (type(payload))
 
+authorization = "Bearer " + os.environ["mail_api_key"] 
+
 headers = {
-    'authorization': "Bearer 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",
+    'authorization': authorization,
     'content-type': "application/json"
     }
 
@@ -17,3 +22,4 @@ res = conn.getresponse()
 data = res.read()
 
 print(data.decode("utf-8"))
+
