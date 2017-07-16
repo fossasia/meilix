07cba998dd3caef5a077d345202a684433bddfa5
Xeon Zolt <xeon.harsh@gmail.com>
2017-07-17 04:08:28 +0530

    added url in the email template

--- mail.py
+++ mail.py
@@ -1,11 +1,15 @@
 #! /usr/bin/env python3
 import http.client
 import os
+import datetime
 
 conn = http.client.HTTPSConnection("api.sendgrid.com")
 
+date = datetime.datetime.now().strftime('%Y%m%d')
+url = "https://github.com/xeon-zolt/meilix/releases/download/"+os.environ["TRAVIS_TAG"]+"/meilix-zesty-"+date+"-i386.iso"
+
 print (os.environ["email"])
-payload = "{\"personalizations\":[{\"to\":[{\"email\":\"" + os.environ["email"] + "\"}],\"subject\":\"Your ISO is Ready\"}],\"from\":{\"email\":\"xeon.harsh@gmail.com\",\"name\":\"Meilix Generator\"},\"reply_to\":{\"email\":\"xeon.harsh@gmail.com\",\"name\":\"Meilix Generator\"},\"subject\":\"Your ISO is ready\",\"content\":[{\"type\":\"text/html\",\"value\":\"<html><p>Hi,<br>Your ISO is ready <br><br>Thank You,<br>Meilix Generator Team</p></html>\"}]}"
+payload = "{\"personalizations\":[{\"to\":[{\"email\":\"" + os.environ["email"] + "\"}],\"subject\":\"Your ISO is Ready\"}],\"from\":{\"email\":\"xeon.harsh@gmail.com\",\"name\":\"Meilix Generator\"},\"reply_to\":{\"email\":\"xeon.harsh@gmail.com\",\"name\":\"Meilix Generator\"},\"subject\":\"Your ISO is ready\",\"content\":[{\"type\":\"text/html\",\"value\":\"<html><p>Hi,<br>Your ISO is ready<br>URL : "+url+"<br><br>Thank You,<br>Meilix Generator Team</p></html>\"}]}"
 
 print (type(payload))
 
