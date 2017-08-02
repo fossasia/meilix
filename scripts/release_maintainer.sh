93a5ccf487d12df4eaf3ba09c29977c11e147805
Abishek V Ashok <abishekvashok@gmail.com>
2017-08-02 10:13:32 +0530

    Adds release_maintainer.sh
    Adds release_maintainer.sh bash script which will fetch all releases and
    will delete all releases that are more than 10 days old.

---
+++ scripts/release_maintainer.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -e
+
+# jq is the JSON parser we will be using
+sudo apt-get install jq
+
+# Storing the response to a variable for future usage
+response=`curl https://api.github.com/repos/fossasia/meilix/releases | jq '.[] | .id, .published_at'`
+
+index=1  # when index is odd, $i contains id and when it is even $i contains published_date
+delete=0 # Should we delete the release?
+current_year=`date +%Y`  # Current year eg) 2001
+current_month=`date +%m` # Current month eg) 2
+current_day=`date +%d`   # Current date eg) 24
+
+for i in $response; do
+    if [ $((index % 2)) -eq 0 ]; then # We get the published_date of the release as $i's value here
+        published_year=${i:1:4}
+        published_month=${i:6:2}
+        published_day=${i:9:2}
+
+        if [ $published_year -lt $current_year ]; then
+             let "delete=1"
+        else
+            if [ $published_month -lt $current_month ]; then
+                let "delete=1"
+            else
+                if [$((current_day-$published_day)) -gt 10 ]; then
+                    let "delete=1"
+                fi
+            fi
+        fi
+    else # We get the id of the release as $i`s value here
+        if [$delete -eq 1]; then
+            curl -X DELETE -H 'Authorization: token XXXXXXXXX' https://api.github.com/repos/fossasia/meilix/releases/$i
+            let "delete=0"
+        fi
+    fi
+    let "index+=1"
+done
