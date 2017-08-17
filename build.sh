60afd43d66e772f0b37f44f272a6fa9d352dc91e
Harsh Lathwal <xeon.harsh@gmail.com>
2017-08-17 23:17:39 +0530

    removed gpicview and gcalculator

--- build.sh
+++ build.sh
@@ -123,8 +123,7 @@ mv /etc/chromium-browser_ /etc/chromium-browser
 apt-get -q -y --purge install lxrandr
 
 # Install Internet packages
-apt-get -q -y --purge install flashplugin-installer google-talkplugin pidgin galculator \
-  gpicview evince libqtwebkit4
+apt-get -q -y --purge install flashplugin-installer google-talkplugin pidgin evince libqtwebkit4
 dpkg -i -y --purge install skype-ubuntu_4.1.0.20-1_i386.deb
 
 # Install graphic
