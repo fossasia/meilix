33203fddac725e12179b16d5ffabb83d9abc7a29
Harsh Lathwal <xeon.harsh@gmail.com>
2017-08-20 03:05:03 +0530

    ubuntu 16.04 onwards dont use /lib for plymouth

--- debuild.sh
+++ debuild.sh
@@ -2,3 +2,7 @@
 rm meilix-default-settings_*                                    #removes the older meilix-default-settings packages
 cd meilix-default-settings                                      #cd into the metapackage directory
 debuild -uc -us                                                 #debuild the meilix-default-settings metapackage
+cd ..
+#buildind plymouth
+cd meilix-artwork                                      #cd into the metapackage directory
+debuild -uc -us                                                 #debuild the plymouth
