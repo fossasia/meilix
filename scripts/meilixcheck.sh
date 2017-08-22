45e87db545e5cdf0c11298dbde302d408811050b
tabesin <tabesin@gmail.com>
2017-08-22 16:20:16 +0200

    Create meilixcheck.sh

---
+++ scripts/meilixcheck.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# A script to provide some raw info about a running meilix system
+# please expand
+
+# ----- Version -----
+lsb_release -a
+
+# ----- Default Browser ------
+# Expected: Chromium
+
+readlink -f /usr/bin/x-www-browser
+update-alternatives --display x-www-browser 
+
+# set it via "sudo update-alternatives --set x-www-browser" etc. 
+
+# ----- default file associations for Video -----
+# Expected VLC
+
+cat /usr/share/applications/defaults.list | grep video
+
+# use xdg-mime to set default mimetype(s)
+
+# ---- no debs in home or root ----
+# Check for leftovers from the install
+
+[ -f ~/*.deb ] && echo "deb package should not be in home folder" || echo "ok."
+[ -f /*.deb ] && echo "deb package should not be in root" || echo "ok."
