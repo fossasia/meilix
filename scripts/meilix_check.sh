e27b5099038e165354436b520e0da1d965c0bc58
tabesin <tabesin@gmail.com>
2017-08-25 17:43:36 +0200

    Update meilix_check.sh

--- scripts/meilix_check.sh
+++ scripts/meilix_check.sh
@@ -30,3 +30,4 @@ cat /usr/share/applications/defaults.list | grep video
 ls /usr/share/plymouth/themes # show us which themes we have
 # show us the plymouth meilix-logo folder
 ls /usr/share/plymouth/themes/meilix-logo/
+ls -l |grep .plymouth
