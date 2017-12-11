06e3c55fe80fcdb07adf156c55024125caa82407
Abishek V Ashok <abhi2424shekvashok@gmail.com>
2017-12-11 21:20:41 +0530

    scripts/release_maintainer.sh: Tweak script
    Tweak the bash script `scripts/release_maintainer.sh` to delete old
    releases which are less than 10 days old.
    
    Signed-off-by: Abishek V Ashok

--- scripts/releases_maintainer.sh
+++ scripts/releases_maintainer.sh
@@ -7,7 +7,18 @@ echo "You have to add an authorization token to make it functional."
 sudo apt-get -qq -y install jq
 
 # Storing the response to a variable for future usage
-response=`curl https://api.github.com/repos/fossasia/meilix/releases | jq '.[] | .id, .published_at'`
+response=`curl https://api.github.com/repos/fossasia/meilix/releases | jq '.[] | .published_at, .id'`
+
+# Sample response:
+#   response = 
+#   "2017-12-11T11:48:12Z"
+#   8851595
+#   "2017-12-11T11:41:52Z"
+#   8851565
+#   "2017-12-11T07:55:12Z"
+#   8848715
+#   "2017-12-01T00:15:29Z"
+#   8727275
 
 index=1  # when index is odd, $i contains id and when it is even $i contains published_date
 delete=0 # Should we delete the release?
@@ -16,26 +27,35 @@ current_month=`date +%m` # Current month eg) 2
 current_day=`date +%d`   # Current date eg) 24
 
 for i in $response; do
-    if [ $((index % 2)) -eq 0 ]; then # We get the published_date of the release as $i's value here
+    if [ $((index % 2)) -eq 1 ]; then # We get the published_date of the release as $i's value here
         published_year=${i:1:4}
         published_month=${i:6:2}
         published_day=${i:9:2}
 
         if [ $published_year -lt $current_year ]; then
-             let "delete=1"
+              echo "Release was made before this year, deleting:"
+              delete=1
         else
+            echo "Release was made this year, trying to compare month"
             if [ $published_month -lt $current_month ]; then
-                let "delete=1"
+                echo "Release was made before more than a month, deleting:"
+                delete=1
             else
+                echo "Release was made this month, trying to compare date"
                 if [ $((current_day-$published_day)) -gt 10 ]; then
-                    let "delete=1"
+                    echo "Release was made before more than 10 days, deleting:"
+                    delete=1
+                else
+                    echo "Release was made within the last 10 days, not deleting:"
+                    delete=0
                 fi
             fi
         fi
     else # We get the id of the release as $i`s value here
+        echo $i
         if [ $delete -eq 1 ]; then
-            curl -X DELETE -H "Authorization: token $KEY" https://api.github.com/repos/fossasia/meilix/releases/$i
-            let "delete=0"
+            curl -u $UNAME:$KEY -X DELETE https://api.github.com/repos/fossasia/meilix/releases/$i
+            delete=0
         fi
     fi
     let "index+=1"
