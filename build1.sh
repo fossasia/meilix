c4d4ff5f60e35147eed3db69a31bfa542d06aab7
Harsh Lathwal <xeon.harsh@gmail.com>
2018-01-17 21:24:53 +0530

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -2,7 +2,7 @@
 #packages required to edit
 sudo apt-get install -qq squashfs-tools genisoimage
 #downloading the ISO to edit
-wget -q https://github.com/fossasia/meilix/releases/download/untagged-ef2fd636a53f4d0268cb/meilix-zesty-20170721-i386.iso
+wget -q https://github.com/fossasia/meilix/releases/download/untagged-47926c1ccbcdf691120b/meilix-zesty-20180104-i386.iso
 mv *.iso meilix-original.iso
 #exit on any error
 set -e
