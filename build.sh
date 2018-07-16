8b602d661d045d5a47cd3223eeb272f74d25e6d9
tabesin <tabesin@gmail.com>
2018-07-16 22:50:43 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -43,10 +43,6 @@ sudo apt-get -qq install dpkg-dev debhelper fakeroot
 sudo apt-get -qq install devscripts
 sudo apt-get -qq install tree # for debugging
 
-# Test out what happens if we don't have the image folder anymore
-mv image image2
-mkdir image
-
 # Adding Mew to the Meilix
 # chmod +x ./scripts/mew.sh
 #./scripts/mew.sh
