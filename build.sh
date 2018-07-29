1ff60d890745f476d44e29a16b1ce033e0ab39fa
tabesin <tabesin@gmail.com>
2018-07-29 11:04:51 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -108,16 +108,17 @@ echo $0: Preparing image...
 [ -d image ] && sudo /bin/rm -r image
 tar xvvf image-${arch}.tar.lzma
 
-# debug for blackscreen issue
-#ls -a chroot/boot
+ls -a chroot/boot
 
 # Copy the kernel from the chroot into the image for the LiveCD
-sudo \cp -rf chroot/boot/vmlinuz-**-generic image/casper/vmlinuz
-sudo \cp -rf chroot/boot/initrd.img-**-generic image/casper/initrd.lz
+sudo \cp --verbose -rf chroot/boot/vmlinuz-**-generic image/casper/vmlinuz
+sudo \cp --verbose -rf chroot/boot/initrd.img-**-generic image/casper/initrd.lz
 
 
 echo debug, Check the contents
 7z l image/casper/initrd.lz
+file image/casper/initrd.lz 
+
 
 # Extract initrd and update uuid configuration
 7z e image/casper/initrd.lz && \
