1639b3cecbe98450b1a040dd7f39daff3611b6a4
tabesin <tabesin@gmail.com>
2018-08-07 13:36:17 +0200

    Update legacy_initrdext.sh

--- scripts/legacy_initrdext.sh
+++ scripts/legacy_initrdext.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # File includes some legacy hints on dealing with the compressed init ramdisk formats
+# Debian provides some useful documentation on this.
 
 # General info.
- 
+
 # 7z l image/casper/initrd.lz
 # file image/casper/initrd.lz 
 # lzcat -dS .lz image/casper/initrd.lz | cpio -iv
@@ -23,7 +24,10 @@
 #  cp initrd_FILES/conf/uuid.conf image/.disk/casper-uuid-generic && \
 #  rm -R initrd_FILES/
 
-For complex for case 2 and update uuid configuration (handled in build.sh)
+# For complex initrd case 2 (handled in build.sh)
 # file initrd.lz outputs ASCII cpio archive (SVR4 with no CRC)
-# see also 7z l image/casper/initrd.lz which displays a block on top.
+# when you extract ordinary you get a directory kernel and microcode image
+# see also 7z l image/casper/initrd.lz which displays the block on top.
 # (cpio -idvm; zcat | cpio -idvm) < initrd.lz
+# should get you the actual initramdisc
+# in the build.sh we use another method with a bitwalk dependency.
