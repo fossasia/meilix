6d926717a8056764bd25d267080f1212f673922a
tabesin <tabesin@gmail.com>
2018-08-13 22:27:59 +0200

    Update debuild.sh

--- scripts/debuild.sh
+++ scripts/debuild.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo In which directory this is started?
-pwd
+# In which directory is this script started? It is supposed to be in the project folder.
+# on travis in /home/travis/build/fossasia/meilix
 
-rm meilix-default-settings_*                                    #removes the older meilix-default-settings packages
+rm meilix-default-settings_*                                    #removes the older meilix-default-settings packages if exist
 cd meilix-default-settings                                      #cd into the metapackage directory
 debuild -uc -us                                                 #debuild the meilix-default-settings metapackage
 cd ..
-#buildind plymouth
+#building plymouth
 sudo apt-get -qq install libfile-fcntllock-perl  #installing files required by meilix artwork to build plymouth
 cd meilix-artwork                                      #cd into the metapackage directory
 echo y | debuild -uc -us                                                 #debuild the plymouth
