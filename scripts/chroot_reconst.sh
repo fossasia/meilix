9fe25b4b202b0cba69a49719e830a872c4a23e46
tabesin <tabesin@gmail.com>
2018-11-14 23:21:10 +0100

    First commit chroot reconstruction

---
+++ scripts/chroot_reconst.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Minimimal chroot file for a reconstruction of meilix
+# 14 Nov 18 v.0.1 first minimal try
+
+sudo chroot chroot <<EOF
+# Set up several useful shell variables
+export CASPER_GENERATE_UUID=1
+export HOME=/root
+export TTY=unknown
+export TERM=vt100
+export DEBIAN_FRONTEND=noninteractive
+export LANG=C
+export LIVE_BOOT_SCRIPTS="casper lupin-casper"
+
+# To allow a few apps using upstart to install correctly. JM 2011-02-21
+dpkg-divert --local --rename --add /sbin/initctl
+ln -s /bin/true /sbin/initctl
+
+# Installing wget
+apt-get install wget apt-transport-https
+
+# Add key for third party repo
+apt-key update 
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E1098513
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1EBD81D9
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 91E7EE5E
+
+# Update in-chroot package database
+apt-get -qq update
+
+# Install core packages
+apt-get -qq -y --purge install ubuntu-standard casper lupin-casper \
+  laptop-detect os-prober linux-generic
+
+# Install base packages
+apt-get -qq -y install xorg  
+apt-get -qq -y install sddm
+# Install LXQT components
+apt-get -qq -y --allow-unauthenticated install lxqt openbox 
+
+# Meilix Check Skript
+chmod +x meilix_check.sh
+./meilix_check.sh
+
+# Reverting earlier initctl override. JM 2012-0604
+rm /sbin/initctl
+dpkg-divert --rename --remove /sbin/initctl
+
+exit
+EOF
