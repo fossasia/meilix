3532c2fb0b02ecde7ba710d2fe7de2162cb8a8e5
Pranav Kulshrestha <hulkcodexl16@gmail.com>
2019-01-19 21:33:43 +0530

    Added git to the custom ISO (#375)

--- scripts/chroot_reconst.sh
+++ scripts/chroot_reconst.sh
@@ -60,6 +60,9 @@ apt-get -qq -y remove xscreensaver
 
 # plymouth boot splash
 
+# Installing 
+ap-get -qq -y install git
+
 # Installing sublime text editor
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
